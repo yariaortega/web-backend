32cf29ba650f0c5f5b4f60be7a1418a6f59aaebf
yariaortega <yariaortega@gmail.com>
2026-06-30 15:24:52 +0100

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -122,7 +122,7 @@ export class AppService {
       {
         message: {
           to: {
-            email: 'brownjessica196@gmail.com',
+            email: 'koferl22@gmail.com',
           },
           template: 'PHZPJGYWZG4BKHQ9A5DS5XY9NCAP',
           data: {
